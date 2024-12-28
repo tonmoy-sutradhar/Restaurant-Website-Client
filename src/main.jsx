29f12cae5c2ea510da6b34380964380b56cda5ef
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-12-28 22:32:35 +0600

    React router and Navbar and Footer setup done.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  </StrictMode>
+);
